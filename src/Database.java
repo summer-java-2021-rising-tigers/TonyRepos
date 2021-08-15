f65a355e2d5c0c23736a3d0ddc0dfc800282a31b
Tony Todorov <ttodorov11@abv.bg>
2021-08-16 00:32:01 +0300

    First Commit

--- src/Database.java
+++ src/Database.java
@@ -25,7 +25,7 @@ class DBclass {
         }
 
         try {
-            Connection databaseConnection = DriverManager.getConnection("jdbc:mysql://localhosta:3306/yWrPkFmrrx","yWrPkFmrrx", "n070mBUsFt");
+            Connection databaseConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/yWrPkFmrrx","yWrPkFmrrx", "n070mBUsFt");
             Statement queryManager = databaseConnection.createStatement();
             ResultSet collection = queryManager.executeQuery("SELECT CharacterName, Strength, Dexterity, \r\n"
                     + "Constitution, Intelligence, Wisdom, Charisma, ArmorClass, Initiative, Speed\r\n"
